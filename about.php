270dc1f9d1ac133f8b8272e8176cf23551ecca11
akkichaubey <akashakki9520@gmail.com>
2024-12-24 23:47:25 +0530

    About section service section done

---
+++ about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include ("include/head.php"); ?>
+</head>
+
+<body>
+  <!-- header section start -->
+  <?php include ("include/header.php"); ?>
+  <!-- header section end -->
+
+  <!-- hero-banner -->
+  <div class="banner inner-banner">
+    <div class="banner-bg img-cover">
+      <img src="./assets/images/inner-bg.jpg" alt="banner" width="" height="">
+    </div>
+    <div class="container">
+      <div class="banner-info">
+        <h1 class="h1 banner-title">About Us</h1>
+        <ul class="breadcrumb">
+          <li><a href="index.php">Home</a></li>
+          <li>About Us</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- hero-banner -->
+  <!-- about -->
+  <div class="section about-wrap pt-148 pb-148">
+    <div class="container">
+      <div class="title-wrap pb-100 text-center">
+        <div class="sub-title h4">Sumar Biotech</div>
+        <h2 class="h2">We are committed to quality</h2>
+      </div>
+      <div class="row row-gap-30 align-items-center">
+        <div class="col-lg-6 pe-lg-5">
+          <div class="content-box pe-lg-4">
+            <p><strong>SUMAR Biotech LLP, registered in January 2018, is a startup approved by Gujarat State
+                Biotechnology Mission. SUMAR Biotech LLP was started by emerging entrepreneurs and a team of young but
+                experienced research scientists and manufacturing professionals.</strong></p>
+            <p>We work together to innovate and manufacture Semi-synthetic, synthetic and Fermentation API. Our R&D set
+              up is equipped with state-of-the-art facility for synthesis, extraction, isolation and purification of
+              products from upstream (fermentation) process.
+            </p>
+            <p>Our team is also involved in developing and implementation of new ideas in process chemistry for
+              different therapeutic molecules to help our clients deal with the competitive market. We have well
+              established analytical lab to support R & D Department, impurity isolation from complex compounds.</p>
+            <p>With state-of-the-art facility at our plant, we are well focused on robust growth in the coming years.
+            </p>
+            <div class="btn-wrap pt-54">
+              <a href="#" class="btn">Explore More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 ps-lg-0">
+          <div class="about-img shape">
+            <img src="./assets/images/about.png" alt="about" width="" height="">
+            <div class="icon-box">
+              <img src="./assets/images/icon-1.svg" alt="icon" width="" height="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- about -->
+
+  <!-- footer section start -->
+  <?php include ("include/footer.php"); ?>
+  <?php include ("include/script.php"); ?>
+  <!-- footer section end -->
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -25,7 +25,7 @@
             research and manufacturing excellence.</p>
         </div>
         <div class="btn-wrap">
-          <a href="#" class="btn">Learn More</a>
+          <a href="#" class="btn">Explore Our Expertise</a>
         </div>
       </div>
     </div>
@@ -34,7 +34,170 @@
     </div>
   </div>
   <!-- hero-banner -->
-  <!-- hero-banner -->
+  <!-- about -->
+  <div class="section about-wrap pt-148 pb-148">
+    <div class="container">
+      <div class="row row-gap-30 align-items-center">
+        <div class="col-lg-6 pe-lg-5">
+          <div class="title-wrap pb-60">
+            <div class="sub-title h4">Sumar Biotech</div>
+            <h2 class="h2">We are committed to quality</h2>
+          </div>
+          <div class="content-box pe-lg-4">
+            <p><strong>At SUMAR Biotech LLP</strong>, quality is at the heart of everything we do. Founded
+              in 2018 and
+              approved by the
+              Gujarat State Biotechnology Mission, our company is driven by a team of emerging
+              entrepreneurs and
+              experienced scientists. Together, we specialize in the development and manufacture of
+              semi-synthetic,
+              synthetic, and fermentation-based Active Pharmaceutical Ingredients (API).</p>
+            <div class="qoute-box">
+              <p>We employ state-of-the-art facilities for synthesis, extraction, isolation, and
+                purification,
+                ensuring
+                the highest standards for each product. Our dedicated analytical lab supports our R&D
+                department,
+                enabling
+                impurity isolation from complex compounds and continuous innovation in process chemistry
+                for various
+                therapeutic molecules.</p>
+            </div>
+            <div class="btn-wrap pt-54">
+              <a href="#" class="btn">Explore More</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 ps-lg-0">
+          <div class="about-img shape">
+            <img src="./assets/images/about.png" alt="about" width="" height="">
+            <div class="icon-box">
+              <img src="./assets/images/icon-1.svg" alt="icon" width="" height="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- about -->
+
+  <!-- service -->
+  <div class="section service-wrap">
+    <div class="inner pt-140 pb-140">
+      <div class="container">
+        <div class="title-btn-wrap pb-70">
+          <div class="title-wrap">
+            <div class="sub-title">We offer an extensive selection of biotechnology </div>
+            <h2 class="h2">Products</h2>
+          </div>
+          <div class="btn-wrap">
+            <a href="#" class="btn">Explore More</a>
+          </div>
+        </div>
+        <div class="service-list">
+          <ul class="row">
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-1.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Antifungal</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-2.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Antibiotics</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-3.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Oncology</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-4.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Cardio Vascular</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-5.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Immunosuppressant</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-6.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Others</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-7.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Fermentation API</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+            <li class="col-xl-3 col-lg-4 col-md-6 col-12">
+              <div class="service-box">
+                <div class="service-icon">
+                  <img src="./assets/images/service-8.svg" alt="icon" width="" height="">
+                </div>
+                <h3 class="h3">Intermediates</h3>
+                <p>Our antibiotics portfolio offers a range of innovative solutions to combat bacterial infections.</p>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-link">Know More</a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- service -->
 
 
   <!-- footer section start -->
